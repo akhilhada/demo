5a8a3a92fe08f47fff466ca6becb4a9975560aa8
akhilhada <147588@01HW966168.India.TCS.com>
2019-08-12 12:55:02 +0530

    ok

---
+++ src/main/java/com/poc/dev/ApipocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipocApplication.class, args);
+	}
+
+}
